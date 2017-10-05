1863501a99a5c47abd6e2b2c941921e6a3fb7e60
Tera Byte aka Gurkirat Singh <tbhaxor@gmail.com>
2017-10-06 03:22:23 +0530

    PHP MySQL - Student Management I (procedural mysql)

---
+++ PHP MySQL - Student Management I/config.php
@@ -0,0 +1,17 @@
+<?php
+/*
+    edit this file according to your needs
+*/
+
+// CONFIGURING DATA BASE
+  $dbhost = "localhost";
+  $dbname = "test";
+  $dbuser = "root";
+  $dbpass = "";
+
+// CONFIGURING PERSONAL DETAILS
+  $coder = "&lt;ENTER CODER NAME&gt;";
+  $institution = "&lt;ENTER INSTITUTION NAME&gt;";
+  $project = "PHP MYSQL - Student Management Project";
+  $submited_to = "&lt;ENTER TEACHER NAME&gt;";
+?>
---
+++ PHP MySQL - Student Management I/index.php
@@ -0,0 +1,8 @@
+<!--IMPORT STYLESHEET AND SCRIPT-->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+
+<?php
+// PHP BACKEND and FrontEnd
+
+?>
