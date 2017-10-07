6107ccd091c8ea91344b1362aafc4da1398cd8fd
Tera Byte aka Gurkirat Singh <tbhaxor@gmail.com>
2017-10-08 02:15:10 +0530

    C - Phone Book Management (pointer demo [stacks])

--- C - Phone Book Management/main.c
+++ C - Phone Book Management/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
+struct phbook
+{
+	char name[50],email[50],address[50],number[10];
+	phbook *link;
+};
+int main() 
+{
+	int id=0,opt;
+	printf("<----=[Info]=---->\n\n\n");
+	printf("Project : Phone Book Management Program [using stacks]\n");
+	printf("Coded By : <enter your name>\n");
+	printf("Submitted To : <enter teacher name>\n");
+	printf("Institution / College : <enter institution / college name>\n");
+	while(true)
+	{
+		system("cls");
+		
+			
+	}
 	return 0;
 }
