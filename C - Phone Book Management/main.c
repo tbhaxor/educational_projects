e5c9d76f460307f2543f7675d57c0755f3800245
Tera Byte aka Gurkirat Singh <tbhaxor@gmail.com>
2017-10-08 02:19:48 +0530

    C - Phone Book Management (pointer demo [queue])

--- C - Phone Book Management/main.c
+++ C - Phone Book Management/main.c
@@ -3,19 +3,26 @@
 struct phbook
 {
 	char name[50],email[50],address[50],number[10];
-	phbook *link;
+	struct phbook *link;
 };
 int main() 
 {
 	int id=0,opt;
 	printf("<----=[Info]=---->\n\n\n");
-	printf("Project : Phone Book Management Program [using stacks]\n");
+	printf("Project : Phone Book Management Program [using queue]\n");
 	printf("Coded By : <enter your name>\n");
 	printf("Submitted To : <enter teacher name>\n");
-	printf("Institution / College : <enter institution / college name>\n");
-	while(true)
+	printf("Institution / College : <enter institution / college name>\n\n\n");
+	system("pause");
+	while(1)
 	{
 		system("cls");
+		printf("----- Menu -----\n\n1. Add New Entry");
+		printf("\n2. Delete Entry");
+		printf("\n3. List All");
+		printf("\n3. List All");
+		
+		
 		
 			
 	}
