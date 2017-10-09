a5d41d268a933653f12f8d29be68687621db4cb2
Tera Byte aka Gurkirat Singh <tbhaxor@gmail.com>
2017-10-09 15:44:23 +0530

    PHP MySQL - Hotel Management I (procedural mysql php)

---
+++ PHP MySQL - Hotel Management I/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="assets/css/master.css">
+    <title>Hotel Management Program</title>
+  </head>
+  <body style="background-image:url('assets/media/bg.jpg');height: 100%;background-attachment: fixed;background-position: center;background-repeat: no-repeat;background-size: cover;">
+    <div class="container">
+      <header class="jumbotron black-white margin-top-10 margin-bottom-20">
+        <h4 class="text-center header margin-top-m15">Project -> Hotel Management Application</h4>
+        <h4 class="text-center header">Coded By -> Gurkirat Singh</h4>
+        <h4 class="text-center header">Submitted To -> TEACHER :P</h4>
+        <h4 class="text-center header margin-bottom-m15">University Name -> PTU</h4>
+      </header>
+      <section id=portal>
+        <h5 class="text-center" id=portal>User Login / Sign up Portal</h5>
+      </section>
+    </div>
+  </body>
+</html>
