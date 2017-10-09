cd4f2c6059b516d474c6d8fd7ff765cb4386a83d
Tera Byte aka Gurkirat Singh <tbhaxor@gmail.com>
2017-10-09 13:45:53 +0530

    lol

--- PHP MySQL - Student Management III/dbconnection.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once 'config.php';
-$connection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
-?>
