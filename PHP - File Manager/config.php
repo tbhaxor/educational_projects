18e50647846786869c22555caea379ebfa173a1b
Tera Byte aka Gurkirat Singh <tbhaxor@gmail.com>
2017-10-10 02:28:07 +0530

    PHP - FILE MANAGER

---
+++ PHP - File Manager/config.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  change according to your requrements
+*/
+$coder = "&lt;ENTER NAME HERE&gt;";
+$institution = "&lt;ENTER NAME HERE&gt;";
+$submitted_to = "&lt;ENTER NAME HERE&gt;";
+$password = "password";
+?>
