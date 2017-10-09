902d619d777a825cdb777310f7c52897a322a418
Tera Byte aka Gurkirat Singh <tbhaxor@gmail.com>
2017-10-09 23:00:40 +0530

    PHP - File Manager

--- PHP - File Manager/index.php
+++ PHP - File Manager/index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="utf-8">
     <title>PHP File Manager</title>
   </head>
   <body>
-    
+    <header>
+      <?php include_once 'config.php';?>
+    </header>
   </body>
 </html>
