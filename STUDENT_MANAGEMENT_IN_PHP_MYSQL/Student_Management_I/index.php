a3aa8459bbb0feec5221dc3583583ee73e200662
tbhaxor <tbhaxor@gmail.com>
2017-10-16 12:28:29 +0530

    comments added

--- STUDENT_MANAGEMENT_IN_PHP_MYSQL/Student_Management_I/index.php
+++ STUDENT_MANAGEMENT_IN_PHP_MYSQL/Student_Management_I/index.php
@@ -18,77 +18,128 @@ if(isset($_POST['login']))   // checking if user wants to login ?
   include_once 'dbconnection.php';  // including db connection file
   if($connection)   // checking whether the client - server dbconnection is established or not
   {
-    $name = $_POST['inuser'];
-    $pass = $_POST['inpass'];
-    $sql = "SELECT * FROM `stumanage_users` WHERE name='$name' and passwd='$pass'";
-    $result = @mysqli_query($connection,$sql);
-    if($result)
+    /*
+     * Now here client is connected with data base
+     */
+    $name = $_POST['inuser'];  // getting username entered by the user
+    $pass = $_POST['inpass'];  // getting userpass entered by the user
+    $sql = "SELECT * FROM `stumanage_users` WHERE name='$name' and passwd='$pass'";  // sql query
+    $result = @mysqli_query($connection,$sql);   // running sql query on connection
+    if($result)  // checking whether the query executed or not
     {
-      $row  = mysqli_fetch_assoc($result);
-      if($row)
+      /*
+       * query executed successfully
+       */
+      $row  = @mysqli_fetch_assoc($result);  // fetching the tuples from the query
+      if($row)   // checking whether entered username and password exists or not
       {
-        $_SESSION['slogin'] = true;
-        $_SESSION['suser'] = $name;
+        /*
+         * user exists
+         */
+        $_SESSION['slogin'] = true; // setting session
+        $_SESSION['suser'] = $name;  // setting username
       }
       else
       {
+        /*
+         * this says user does not exists
+         */
         echo "<script>alert('Error : User $name is not registered');</script>";
       }
     }
+
     else
     {
+      /*
+       * Now here client is connected with data base
+       */
       echo "<script>alert('Error : Can\'t Run Query');</script>";
     }
-    mysqli_close($connection);
+
+    mysqli_close($connection); // closing the mysql connection
   }
   else
   {
+    /*
+     * connection to data base was not established
+     */
     echo "<script>alert('Error : Can\'t Establish DB Connection');</script>";
   }
 }
-if(isset($_POST['signup']))
+if(isset($_POST['signup'])) // checking whether user wants to be added
 {
-include_once 'dbconnection.php';
-if($connection)
-{
-  $name = $_POST['upuser'];
-  $pass = $_POST['uppass'];
-  $name = str_replace("<","&lt;",$name);
-  $name = str_replace(">","&gt;",$name);
-  $pass = str_replace(">","&gt;",$pass);
-  $pass = str_replace("<","&lt;",$pass);
-  $sql = "INSERT INTO `stumanage_users`(`name`, `passwd`) VALUES ('$name','$pass')";
-  $result = @mysqli_query($connection,$sql);
-  if($result)
+  /*
+   * connection to data base was not so
+   */
+  include_once 'dbconnection.php'; // including dbconnection, trying to establish the connection
+  if($connection)  // checking whether client server database connection is established
   {
-      echo "<script>alert('Error : User $name is registered');</script>";
+    /*
+     * connection to database is established
+     */
+    $name = $_POST['upuser'];  // getting username from user
+    $pass = $_POST['uppass']; // getting password from user
+
+    // applied security patch to prevent from XSS
+    $name = str_replace("<","&lt;",$name);
+    $name = str_replace(">","&gt;",$name);
+    $pass = str_replace(">","&gt;",$pass);
+    $pass = str_replace("<","&lt;",$pass);
+
+    $sql = "INSERT INTO `stumanage_users`(`name`, `passwd`) VALUES ('$name','$pass')"; // sql query
+    $result = @mysqli_query($connection,$sql);  // running sql query on connection
+    if($result)  // checks whether query successfully executed or not
+    {
+      /*
+       * data successfully inserted
+       */
+        echo "<script>alert('Error : User $name is registered');</script>";
+    }
+    else
+    {
+      /*
+       * error while running the sql Query
+       */
+      echo "<script>alert('Error : Can\'t Run Query');</script>";
+    }
+
+    mysqli_close($connection); // closing mysql connection
   }
   else
   {
-    echo "<script>alert('Error : Can\'t Run Query');</script>";
+    /*
+     * connection to data base was not established
+     */
+    echo "<script>alert('Error : Can\'t Establish DB Connection');</script>";
   }
-  mysqli_close($connection);
 }
-else
-{
-  echo "<script>alert('Error : Can\'t Establish DB Connection');</script>";
-}
-}
+// checking if session for slogin is not set then shows the login - sign up portal
 if(!isset($_SESSION['slogin']))
 {
   index();
 }
+// will redirect to dashboard if session for signin exists
 else
 {
   panel();
 }
+/*
+ *  function to redirect to dashboard if user is signed or already signed in
+ */
 function panel()
 {
-  header("location:manager/dashboard.php");
+  header("location:manager/dashboard.php"); // header function is used to in
 }
+
+/*
+ *  function to view login / signup portal
+ */
 function index()
 {
+  // including all the variables for info.php
   include_once 'info.php';
+
+  // printing the front end
   echo "<center><div class='container margin-bottom-20 jumbotron header'><h2 class=top>Project Name : ".@$project."<br>Coded By : ".@$coder."<br>Submitted To :".@$submited_to."</h1></div>";
   echo "<form method=post action='' class=form-horizontal>
   <div class=container>
--- STUDENT_MANAGEMENT_IN_PHP_MYSQL/Student_Management_I/info.php
+++ STUDENT_MANAGEMENT_IN_PHP_MYSQL/Student_Management_I/info.php
@@ -1,6 +1,6 @@
 <?php
-  $coder = "&lt;ENTER YOUR NAME&gt;";
-  $institution = "&lt;ENTER INSTITUTION NAME&gt;";
-  $project = "PHP MYSQL - Student Management Project";
-  $submited_to = "&lt;ENTER TEACHER NAME&gt;";
+  $coder = "&lt;ENTER YOUR NAME&gt;";  // name of programmer
+  $institution = "&lt;ENTER INSTITUTION NAME&gt;";  // institution name
+  $project = "PHP MYSQL - Student Management Project";  // project name
+  $submited_to = "&lt;ENTER TEACHER NAME&gt;";  // TEACHER name 
 ?>
