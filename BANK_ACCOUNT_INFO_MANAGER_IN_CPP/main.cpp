2ee5051ad399998ed14019498ada738684426b51
tbhaxor <tbhaxor@gmail.com>
2017-10-19 06:39:39 +0530

    deleted some uncompleted

--- BANK_ACCOUNT_INFO_MANAGER_IN_CPP/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "bank.h"
-#include "config.cpp"
-int main() 
-{
-	return 0;
-}
--- FILE_MANAGER_IN_PHP/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-  change according to your requrements
-*/
-$coder = "&lt;ENTER NAME HERE&gt;";
-$institution = "&lt;ENTER NAME HERE&gt;";
-$submitted_to = "&lt;ENTER NAME HERE&gt;";
-$password = "password";
-?>
