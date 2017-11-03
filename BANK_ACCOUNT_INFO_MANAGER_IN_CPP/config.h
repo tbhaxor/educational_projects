46c6cf2a818900c115fcfcf29afdbf6f59ccb6d2
tbhaxor <tbhaxor@gmail.com>
2017-11-04 01:55:03 +0530

    new program added

---
+++ BANK_ACCOUNT_INFO_MANAGER_IN_CPP/config.h
@@ -0,0 +1,19 @@
+#ifndef INITS_H_
+#define INITS_H_
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+namespace Config
+{
+  // config variables
+  string coder = "Gurkirat Singh";
+  string submitted_to = "Mrs. Seema Sehgal";
+  string institution = "MDU";
+
+  void info()
+  {
+    cout<<system('cls');
+  }
+}
+#endif // Inits
---
+++ BANK_ACCOUNT_INFO_MANAGER_IN_CPP/main.cpp
@@ -0,0 +1,15 @@
+// including standard header file an liberar
+#include <iostream>
+using namespace std;
+
+
+// including local header files and liberaries
+#include "bank.h"
+#include "config.h"
+using namespace Config;
+
+int main()
+{
+  info();
+  return 0;
+}
