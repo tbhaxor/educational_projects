b58f3198c032ceeffc8144b18e9df7135fcc614f
tbhaxor <tbhaxor@gmail.com>
2017-11-24 19:37:09 +0530

    deleted files

--- CPP_AND_POINTERS/STUDENT_INFO_MANAGER_USING_STACK/config.h
+++
@@ -1,8 +0,0 @@
-// Programer Info
-#define CODER "Gurkirat Singh"
-#define SUBMITTED_TO "Mr. Sandeep Singh"
-#define INSTITUTION "MDU Rohtak"
-
-// Program configurations
-#define TOTAL_SUBJECTS 5
-#define CLEAR "clear"
